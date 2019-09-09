29ac088f8a75e1276ed481b98543f2ba6ae85319
André Edvardsson <andedv0222@skynet.halmstad.se>
2019-09-09 13:20:45 +0200

    hfjdsaf

--- 5_sep_lektionen/5_sep_lektionen/Form1.Designer.cs
+++ 5_sep_lektionen/5_sep_lektionen/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 212);
+            this.button1.Location = new System.Drawing.Point(328, 210);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
