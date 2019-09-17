f24a34734eda9cbbf2959ed68939f2efa87dfd15
André Edvardsson <andedv0222@skynet.halmstad.se>
2019-09-17 20:44:35 +0200

    17 Sep

--- 5_sep_lektionen/5_sep_lektionen/Talbas.Designer.cs
+++ 5_sep_lektionen/5_sep_lektionen/Talbas.Designer.cs
@@ -31,39 +31,52 @@
             this.btnBeräkna = new System.Windows.Forms.Button();
             this.tbxTal = new System.Windows.Forms.TextBox();
             this.lblTalbas = new System.Windows.Forms.Label();
+            this.lblSvarD = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnBeräkna
             // 
-            this.btnBeräkna.Location = new System.Drawing.Point(290, 58);
+            this.btnBeräkna.Location = new System.Drawing.Point(12, 12);
             this.btnBeräkna.Name = "btnBeräkna";
-            this.btnBeräkna.Size = new System.Drawing.Size(75, 23);
+            this.btnBeräkna.Size = new System.Drawing.Size(92, 23);
             this.btnBeräkna.TabIndex = 0;
-            this.btnBeräkna.Text = "button1";
+            this.btnBeräkna.Text = "Binärt>Decimal";
             this.btnBeräkna.UseVisualStyleBackColor = true;
             this.btnBeräkna.Click += new System.EventHandler(this.BtnBeräkna_Click);
             // 
             // tbxTal
             // 
-            this.tbxTal.Location = new System.Drawing.Point(290, 105);
+            this.tbxTal.Location = new System.Drawing.Point(12, 41);
             this.tbxTal.Name = "tbxTal";
+            this.tbxTal.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tbxTal.Size = new System.Drawing.Size(100, 20);
             this.tbxTal.TabIndex = 1;
+            this.tbxTal.Text = "0000";
             // 
             // lblTalbas
             // 
             this.lblTalbas.AutoSize = true;
-            this.lblTalbas.Location = new System.Drawing.Point(287, 154);
+            this.lblTalbas.Location = new System.Drawing.Point(50, 64);
             this.lblTalbas.Name = "lblTalbas";
-            this.lblTalbas.Size = new System.Drawing.Size(35, 13);
+            this.lblTalbas.Size = new System.Drawing.Size(16, 13);
             this.lblTalbas.TabIndex = 2;
-            this.lblTalbas.Text = "label1";
+            this.lblTalbas.Text = "---";
+            // 
+            // lblSvarD
+            // 
+            this.lblSvarD.AutoSize = true;
+            this.lblSvarD.Location = new System.Drawing.Point(12, 64);
+            this.lblSvarD.Name = "lblSvarD";
+            this.lblSvarD.Size = new System.Drawing.Size(32, 13);
+            this.lblSvarD.TabIndex = 3;
+            this.lblSvarD.Text = "Svar:";
             // 
             // Talbas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSvarD);
             this.Controls.Add(this.lblTalbas);
             this.Controls.Add(this.tbxTal);
             this.Controls.Add(this.btnBeräkna);
@@ -79,5 +92,6 @@
         private System.Windows.Forms.Button btnBeräkna;
         private System.Windows.Forms.TextBox tbxTal;
         private System.Windows.Forms.Label lblTalbas;
+        private System.Windows.Forms.Label lblSvarD;
     }
 }
--- 5_sep_lektionen/5_sep_lektionen/Talbas.cs
+++ 5_sep_lektionen/5_sep_lektionen/Talbas.cs
@@ -19,15 +19,58 @@ namespace _5_sep_lektionen
 
         private void BtnBeräkna_Click(object sender, EventArgs e)
         {
-            string bokstav= tbxTal.Text;
-            for (int i = 0; i < bokstav.Length; i++)
-            {
-                //for (int l = i) ;
-                {
+            string Tal = tbxTal.Text;
+            char A = Tal[0];
+            int A1 = A;
+            int A2 = (A1 - 48) * 16;
+            char B = Tal[1];
+            int B1 = B;
+            int B2 = (B1 - 48) * 8;
+            char C = Tal[2];
+            int C1 = C;
+            int C2 = (C1 - 48) * 4;
+            char D = Tal[3];
+            int D1 = D;
+            int D2 = (D1 - 48) * 2;
+            char E = Tal[4];
+            int E1 = E;
+            int E2 = (E1 - 48) * 1;
+            int SummaD = A2 + B2 + C2 + D2 + E2;
+            //try
+            //{
+            //    int A,B,C,D,E = int.Parse(tbxTal.Text);
+            //}
+            //catch
+            //{
 
-                }
-            }
-            //tbxTal.Text = l;
+            //}
+
+            //string A3 = A2.ToString();
+            lblTalbas.Text = SummaD.ToString();
+
+            //char B = Tal[1];
+            //char C = Tal[0];
+            //char D = Tal[1];
+            //char F = Tal[1];
+
+            //char A2 = (char)A;
+
+
+            //string Fornamn = tbxFornamn.Text;
+            //string Efternamn = tbxEfternamn.Text;
+            //char Finitial = Fornamn[0];
+            //char Einitial = Efternamn[0];
+            //string initialer = Fornamn + " " + Efternamn + " har initialerna " + Finitial.ToString() + "." + Einitial + ".";
+            //lblInitialer.Text = initialer;
+            //    string bokstav= tbxTal.Text;
+            //    for (int i = 0; i < bokstav.Length; i++)
+            //    {
+            //        //for (int l = i) ;
+            //        {
+
+            //        }
+            //    }
+            //    //tbxTal.Text = l;
         }
     }
 }
